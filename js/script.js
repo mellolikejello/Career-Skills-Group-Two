a623a92515618f06e731d3cc1f8f4b7ecc48c79d
Melody <mgk1068@rit.edu>
2014-09-23 02:48:25 -0400

    basic structure for main page - to be styled
    need to add footer, fix styling, fix sticky nav spacing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 window.onload = init;
+window.onscroll = onScrolled;
 
-function init(){
+var didScroll = false;
+
+function init() {
+	setInterval(function() {
+		if(didScroll) {
+			didScroll = false;
+			var header = document.querySelector("header");
+			if(window.scrollY >= (0.95 * window.screen.availHeight)) {
+				if(header.getAttribute("id") == "static-header") {
+					header.setAttribute("id", "sticky-header");
+				}
+			} else {
+				if(header.getAttribute("id") == "sticky-header") {
+					header.setAttribute("id", "static-header");
+				}
+			}
+		}
+	}, 100);
 	
+}
+
+function onScrolled(evt) {
+	didScroll = true;
 }
